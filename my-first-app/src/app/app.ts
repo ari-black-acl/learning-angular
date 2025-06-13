46eaefb692563d8083cd055499c55e991df5be09
ari-black-acl <ariannah.black@americancruiselines.com>
2025-06-13 10:49:51 -0400

    add home component

--- my-first-app/src/app/app.ts
+++ my-first-app/src/app/app.ts
@@ -1,13 +1,27 @@
 import {Component} from '@angular/core';
+import {Home} from './home/home'  // to use in driver (import to driver src code)
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Home],      // import to driver component
   template: `
-    <h1>Hello world!</h1>
+    <main>
+
+      <!-- header, title and logo -->
+      <header class='brand-name'>
+        <img class='brand-logo' src='/assets/logo.svg' alt='logo' aria-hidden='true' />
+      </header>
+
+      <!-- main content -->
+      <section class = 'content'>
+        <app-home></app-home>
+      </section>
+
+    </main>
   `,
   styleUrls: ['./app.css'],
 })
+
 export class App {
   title = 'homes';      // component title
 }
